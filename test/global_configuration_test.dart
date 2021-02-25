fef13674b077534f6761b8d307610ddbfadae116
Tomas Radvansky <radvansky.tomas@gmail.com>
2021-02-25 14:56:37 +1300

    Null-safety migration, add tests for non-deprecated methods

--- test/global_configuration_test.dart
+++ test/global_configuration_test.dart
@@ -18,10 +18,21 @@ void main() {
     expect(GlobalConfiguration().getString("key3"), "value3");
   });
 
+  test('Testing get value for key.', () {
+    GlobalConfiguration().clear();
+    final Map<String, String> config1 = {"key1": "value1", "key2": "value2"};
+
+    final Map<String, String> config2 = {"key3": "value3", "key4": "value4"};
+    GlobalConfiguration().loadFromMap(config1);
+    GlobalConfiguration().loadFromMap(config2);
+    expect(GlobalConfiguration().getValue("key1"), "value1");
+    expect(GlobalConfiguration().getValue("key3"), "value3");
+  });
+
   test('Testing read a deep value', () async {
     GlobalConfiguration().clear();
     final jsonString = '{"values" : { "key1" : "value1", "key2" : "value2" } }';
-    Map config1 = json.decode(jsonString);
+    Map<String, dynamic> config1 = json.decode(jsonString);
 
     GlobalConfiguration().loadFromMap(config1);
     expect(GlobalConfiguration().getDeepValue<String>("values:key2"), "value2");
@@ -30,7 +41,7 @@ void main() {
   test('Testing read a color', () async {
     GlobalConfiguration().clear();
     final jsonString = '{"color" : "#F44336" }';
-    Map config1 = json.decode(jsonString);
+    Map<String, dynamic> config1 = json.decode(jsonString);
 
     GlobalConfiguration().loadFromMap(config1);
     expect(
@@ -56,6 +67,25 @@ void main() {
     fail("Expected Exception!");
   });
 
+  test('Testing updateValue().', () async {
+    GlobalConfiguration().clear();
+    final Map<String, dynamic> config1 = {"hello": "world", "foo": 123};
+    GlobalConfiguration().loadFromMap(config1);
+
+    GlobalConfiguration().updateValue("hello", "world!");
+    expect(GlobalConfiguration().getValue("hello"), "world!");
+    GlobalConfiguration().updateValue("foo", 321);
+    expect(GlobalConfiguration().getValue("foo"), 321);
+
+    try {
+      GlobalConfiguration().updateValue("foo", "321");
+    } catch (e) {
+      expect(GlobalConfiguration().getInt("foo"), 321);
+      return;
+    }
+    fail("Expected Exception!");
+  });
+
   test('Testing update null value.', () async {
     GlobalConfiguration().clear();
     final Map<String, dynamic> config1 = {"hello": "world", "foo": null};
@@ -65,9 +95,24 @@ void main() {
     expect(GlobalConfiguration().getInt("foo"), 321);
   });
 
+  test('Testing update null value.', () async {
+    GlobalConfiguration().clear();
+    final Map<String, dynamic> config1 = {"hello": "world", "foo": null};
+    GlobalConfiguration().loadFromMap(config1);
+
+    GlobalConfiguration().updateValue("foo", 321);
+    expect(GlobalConfiguration().getValue("foo"), 321);
+  });
+
   test('Testing addValue', () async {
     GlobalConfiguration().clear();
     GlobalConfiguration().addValue("hello", "world");
     expect(GlobalConfiguration().getString("hello"), "world");
+  });
+
+  test('Testing addValue', () async {
+    GlobalConfiguration().clear();
+    GlobalConfiguration().addValue("hello", "world");
+    expect(GlobalConfiguration().getValue("hello"), "world");
   });
 }
